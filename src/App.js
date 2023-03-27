a2f07eb92806ac43dc18d1a706a0b2ad7cf2f41d
Heimatbrot <marc-huber@outlook.com>
2023-03-27 15:26:10 +0200

    added next page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Page2 from './components/Page2';
 import Page3 from './components/Page3';
 import Page4 from './components/Page4';
 import Page5 from './components/Page5';
+import Page6 from './components/Page6';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     { id: 3, component: <Page3 /> },
     { id: 4, component: <Page4 /> },
     { id: 5, component: <Page5 /> },
+    { id: 6, component: <Page6 /> },
   ];
 
   const [activePage, setActivePage] = useState(pages[0].id);
