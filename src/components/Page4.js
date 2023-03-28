a72aad11477e72616555005fb53a9a8bf11561a0
Heimatbrot <48zb77j7wg@privaterelay.appleid.com>
2023-03-28 20:18:47 +0200

    page 4 and 5 added

--- src/components/Page4.js
+++ src/components/Page4.js
@@ -1,10 +1,71 @@
 import React from "react";
+import "./Page4.css";
 
 function Page4() {
   return (
-    <div>
-      <h1>This is Page 4</h1>
-      <p>Welcome to Page 4</p>
+    <div id="content">
+      <section class="intro">
+        <h1>DIE GEBOTE DER LINOSO GAMES</h1>
+        <p>Das Orga hat immer Recht.</p>
+      </section>
+      <h3>ARTIKEL 1 (<i>TEAMS</i>)</h3>
+        <p>
+          <strong>1.1</strong> Die Teams werden zufällig gelöst.<br></br>
+          <strong>1.2.1</strong> Die 12 Teilnehmer, werden in sechs zweier oder in vier dreier Teams aufgeteilt (Im ermessen vom Orga Team).<br></br>
+          <strong>1.2.2</strong> Lediglich das Sieger Team der letzten Ausgabe, muss (sofern möglich) als Titelverteidiger unverändert antreten.<br></br>
+          <strong>1.3</strong> Falls ein Teilnehmer verhindert ist, entscheidet das Orga über die neue Aufteilung. Die Losung wird wiederholt.<br></br>
+          <br></br>
+        </p>
+      <h3>ARTIKEL 2 (<i>SPIELE</i>)</h3>
+        <p>
+          <strong>2.1</strong> Es gibt 8 Spiele.<br></br>
+          <strong>2.2</strong> Vier dieser Spiele werden je 2x gespielt.<br></br>
+          <strong>2.3</strong> Vier dieser Spiele werden je 1x gespielt.<br></br>
+          <strong>2.4</strong> Total werden somit 12 Runden gespielt.<br></br>
+          <strong>2.5</strong> Nach dem 6. Spiel findet eine Spielpause statt. Das Orga entscheidet wie lange und kündigt die Wiederaufnahme der Spiele mindestens 5 Minuten davor an.<br></br>
+          <br></br>
+        </p>
+      <h3>ARTIKEL 3 (<i>PUNKTE</i>)</h3>
+        <p>
+          <strong>3.1</strong> Pro Team können Maximal 16 Punkte erreicht werden.<br></br>
+          <strong>3.2</strong> Die Spiele aus 2.2 werden mit jeweils einem Punkt für den Sieger und null Punkte für den Verlierer bewertet.<br></br>
+          <strong>3.3</strong> Die Spiele aus 2.3 ​werden mit jeweils zwei Punkten für den Sieger und null Punkte für den Verlierer bewertet.<br></br>
+          <strong>3.4</strong> Bei Gleichstand am Ende eines Spieles, wird dieses mit Schäre Stein Papier auf 3 entschieden.<br></br>
+          <br></br>
+        </p>
+      <h3>ARTIKEL 4 (<i>SIEGER</i>)</h3>
+        <p>
+          <strong>4.1</strong> Sieger ist das Team, welches am Ende der Spiele die meisten Punkte hat.<br></br>
+          <strong>4.2</strong> Die Siegerehrung wird vom Orgateam abgehalten.<br></br>
+          <strong>4.3</strong> Bei Punktegleichheit wir mithilfe des einzig wahren Spiels ein Sieger entschieden. (Schäre Stein Papier auf 3)!<br></br>
+          <strong>4.4.1</strong> Das Sieger Team darf sich ein Jahr lang (bis zur Austragung der nächsten Spiele!) um die glamuröse goldene Gieskanne kümmern und sich während/nach der Siegerehrung mit den Namen der Teammitglieder darauf verewigen.<br></br>
+          <strong>4.4.2</strong> Die Verewigung muss in einer normalen Schriftgrösse und neben dem Jahresdatum stattfinden.<br></br>
+          <br></br>
+        </p>
+      <h3>ARTIKEL 5 (<i>SONSTIGE BESTIMMUNGEN</i>)</h3>
+        <p>
+          <strong>5.1</strong> Ehre ist eine Sache der Ehre<br></br>
+          <strong>5.2</strong> Unlauteres Verhalten = Strafe<br></br>
+          <strong>5.3</strong> Den Gegner mithilfe von verbalen Äusserungen zu demoralisieren ist erwünscht.<br></br>
+          <strong>5.4</strong> Beim drang, eigene Linoso Games zu veranstalten, muss ein Antrag gestellt werden. Dies geschieht per Kontaktformular (unter FAQ)! Falls jemand eine Genehmigung erhalten sollte, erhält dieser ebenfalls die Vorgefertigten Zeitpläne für beide möglichen Spielmodi (6 Teams/4 Teams, 8 Spiele)<br></br>
+          <strong>5.5</strong> Durch die Teilnahme (also ab start des ersten Spieles) wird das Regelbuch als gelesen und aktzeptiert betrachtet. Es gibt kein nachträgliches Einspracherecht<br></br>
+          <br></br>
+        </p>
+      <h3>ARTIKEL 6 (<i>HAFTUNGSAUSCHLÜSSE</i>)</h3>
+        <p>
+          <strong>6.1</strong> Jegliche Haftung wird abgelehnt.<br></br>
+          <strong>6.2</strong> Die Teilnahme an den Spielen geschieht auf eigene Gefahr.<br></br>
+          <strong>6.3</strong> Durch die Teilnahme, aktzeptiert man automatisch, dass das Orga Team nicht für allfällige Schäden haftbar gemacht werden kann. Wie bereits in Artikel 6.1 und 6.2, muss jeder Teilnehmer selbst für eine Absicherung aufkommen.<br></br>
+          <br></br>
+        </p>
+      <h3>ARTIKEL 69 (<i>ORGA</i>)</h3>
+        <p>
+          <strong>6.1</strong> Das Orga Team besteht aus 3 Personen.<br></br>
+          <strong>6.2</strong> In Streitfällen beratet sich das Orga Team, dieser entscheid wird Demokratisch gefällt und ist Rechtsgültig.<br></br>
+          <strong>6.3</strong> Das Orga Team handelt nicht zwingend im willen der Allgemeinheit.<br></br>
+          <strong>6.4</strong> Das Orga Team entscheidet über die Form und Härte der jeweiligen Strafen.<br></br>
+          <br></br>
+        </p>
     </div>
   );
 }
--- src/components/Page5.js
+++ src/components/Page5.js
@@ -1,10 +1,134 @@
 import React from "react";
+import { useState } from 'react';
+import "./Page5.css";
 
 function Page5() {
+  const [activeAccordion, setActiveAccordion] = useState(null);
+
+  function toggleAccordion(index) {
+    setActiveAccordion(activeAccordion === index ? null : index);
+  }
+
   return (
-    <div>
-      <h1>This is Page 5</h1>
-      <p>Welcome to Page 5</p>
+    <div id="content">
+      <section class="intro">
+        <h1>HÄUFIG GESTELLTE FRAGEN</h1>
+        <p>Frequently Asked Questions (FAQ)</p>
+      </section>
+      <h2>FAQ</h2>
+        <hr />
+      <button
+        className={`accordion ${activeAccordion === 0 ? "active" : ""}`}
+        onClick={() => toggleAccordion(0)}
+      >
+        WARUM GIBT ES DIE LINOSO GAMES?
+      </button>
+      <div className={`panel ${activeAccordion === 0 ? "show" : ""}`}>
+        <p>
+          Die Erfahrung hat gezeigt, dass eine "0815" Geburtstagsfeier immer
+          eintönig und normal ist. Eines frühen Morgens, kam dem Jüngern Linus
+          also die Idee, seine Geburtstagsfeier zu revolutionieren. Das Ziel war
+          es, weg vom Standard zurück in die Vergangenheit zu kommen. Die Idee
+          kein übliches Fest zu feiern war geboren. Doch wie soll das ganze
+          aussehen? Um diese Frage zu klären, kontaktierte er seine beiden
+          Freunde (Sean und Marc), holte ein bisschen Kreativität fördernden
+          Weizen-Sirup. Boom die Linoso Games sind da!
+        </p>
+      </div>
+
+      <button
+        className={`accordion ${activeAccordion === 1 ? "active" : ""}`}
+        onClick={() => toggleAccordion(1)}
+      >
+        WIE WERDEN DIE TEAMS ENTSCHIEDEN?
+      </button>
+      <div className={`panel ${activeAccordion === 1 ? "show" : ""}`}>
+        <p>
+        Zuerst werden die Teams gelost. Lediglich das Sieger Team des vorjahres darf/muss wieder als Team antreten. Ist ein Kandidat verhindert und die Teams gehen nicht mehr auf, kriegt das Sieger Team einen zusätzlichen Spieler (zugelost).
+        </p>
+      </div>
+
+      <button
+        className={`accordion ${activeAccordion === 2 ? "active" : ""}`}
+        onClick={() => toggleAccordion(2)}
+      >
+        WER ENTSCHEIDET ÜBER DIE SPIELE UND DEREN REGELN?
+      </button>
+      <div className={`panel ${activeAccordion === 2 ? "show" : ""}`}>
+        <p>
+          Diese werden vom Orga Team geplant und gemeinsam festgelegt.
+        </p>
+      </div>
+
+      <button
+        className={`accordion ${activeAccordion === 3 ? "active" : ""}`}
+        onClick={() => toggleAccordion(3)}
+      >
+        WER IST DAS ORGA TEAM?
+      </button>
+      <div className={`panel ${activeAccordion === 3 ? "show" : ""}`}>
+        <p>
+          Linus, Sean und Marc.
+        </p>
+      </div>
+
+      <button
+        className={`accordion ${activeAccordion === 4 ? "active" : ""}`}
+        onClick={() => toggleAccordion(4)}
+      >
+        KANN ICH AUCH TEILNEHMEN?
+      </button>
+      <div className={`panel ${activeAccordion === 4 ? "show" : ""}`}>
+        <p>
+          Keine Einladung, keine Teilnahme, tut uns leid!  
+        </p>
+      </div>
+
+      <button
+        className={`accordion ${activeAccordion === 5 ? "active" : ""}`}
+        onClick={() => toggleAccordion(5)}
+      >
+        DARF ICH DIE LINOSO GAMES NACHAHMEN?
+      </button>
+      <div className={`panel ${activeAccordion === 5 ? "show" : ""}`}>
+        <p>
+          Für dieses Anliegen bitte kontaktieren Sie das Orga Team per Kontaktforumar!
+        </p>
+      </div>
+
+      <button
+        className={`accordion ${activeAccordion === 6 ? "active" : ""}`}
+        onClick={() => toggleAccordion(6)}
+      >
+        WIE LANGE DAUERT EINE ANTWORT PER KONTAKTFORULAR?
+      </button>
+      <div className={`panel ${activeAccordion === 6 ? "show" : ""}`}>
+        <p>
+        Dies können wir leider nicht definitiv sagen. Unser Posteingang wird nicht regelmässig überprüft.  
+        </p>
+      </div>
+
+
+
+      <h2>KONTAKT</h2><hr></hr>
+      <form
+      action="https://formspree.io/f/mdovzzkw"
+      method="POST"
+      className="formlayout">
+        <label className="formhead">
+          Ihre Email Adresse:<br />
+          <input type="email" name="email" className="forminput" />
+        </label>
+          <br />
+        <label className="formhead">
+          Ihre Anfrage:<br />
+          <textarea name="message" className="formtext" />
+        </label>
+          <br />
+        <button type="submit" className="formbutton">
+          Absenden
+        </button>
+    </form>
     </div>
   );
 }
